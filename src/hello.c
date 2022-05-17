fdd2d411afdfdbbbc7772860e31395cc639b40ed
Hwangwoosam <sam1783@naver.com>
2022-05-17 22:15:58 +0900

    modify hello.c

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void foo1(){
-	printf("foo1\n");
+	printf("foo1-see\n");
+	
 }
 
 void foo2(){
@@ -9,7 +10,7 @@ void foo2(){
 }
 
 void foo3(){
-	printf("foo3\n");
+	printf("foo3-seen\n");
 }
 int main(){
 
