a9cae8c3980ea7d469089bcfab6ffc7f9a170177
Hwangwoosam <sam1783@naver.com>
2022-05-24 23:16:19 +0900

    code change

--- test/cJSON/cJSON.c
+++ test/cJSON/cJSON.c
@@ -239,7 +239,8 @@ CJSON_PUBLIC(void) cJSON_InitHooks(cJSON_Hooks* hooks)
 
 /* Internal constructor. */
 static cJSON *cJSON_New_Item(const internal_hooks * const hooks)
-{
+{   
+    //code change test1
     cJSON* node = (cJSON*)hooks->allocate(sizeof(cJSON));
     if (node)
     {
@@ -303,7 +304,8 @@ typedef struct
 
 /* Parse the input text to generate a number, and populate the result into item. */
 static cJSON_bool parse_number(cJSON * const item, parse_buffer * const input_buffer)
-{
+{   
+    //code change test1
     double number = 0;
     unsigned char *after_end = NULL;
     unsigned char number_c_string[64];
