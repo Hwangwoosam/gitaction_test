855a14a258751f45442b98e0a022852970681d6a
Hwangwoosam <61729954+Hwangwoosam@users.noreply.github.com>
2022-05-25 14:19:46 +0900

    Update script.py

--- script.py
+++ script.py
@@ -13,26 +13,26 @@ def execute_aflpp(aflpp_path,executable_name,local_seeddir_path,file_mode):
     afl_fuzz_path = aflpp_path + "/afl-fuzz"
     cmd = [afl_fuzz_path,"-i",local_seeddir_path,"-o","local_out",executable_name]
     if file_mode == True:
-      cmd.append("@@")
+        cmd.append("@@")
 
     env_var = os.environ.copy()
     env_var["AFL_SKIP_CPUFREQ"] = "1"
     env_var["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
 
     if (sys.version_info < (3,3)):
-      proc = subprocess.Popen(cmd,env=env_var)
-      timer = Timer(5,proc.kill)
-      try:
-        timer.start()
-        proc.wait()
-      finally:
-        timer.cancel()
+       proc = subprocess.Popen(cmd,env=env_var)
+       timer = Timer(5,proc.kill)
+       try:
+         timer.start()
+         proc.wait()
+       finally:
+         timer.cancel()
     else:
-      proc = subproess.Popen(cmd, env=env_var)
-      try:
-        proc.wait(timeout=5)
-      except subprocess.TimeoutExpired:
-        proc.kill()
+       proc = subprocess.Popen(cmd, env=env_var)
+       try:
+         proc.wait(timeout=5)
+       except subprocess.TimeoutExpired:
+         proc.kill()
 
     return proc.returncode
  
