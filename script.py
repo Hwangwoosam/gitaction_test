c4d28ef5fdb36ca300dddad8fc89e42a6aa83ed1
Hwangwoosam <61729954+Hwangwoosam@users.noreply.github.com>
2022-05-25 10:39:07 +0900

    Update script.py

--- script.py
+++ script.py
@@ -9,32 +9,32 @@ import subprocess
 from threading import Timer
 
 def execute_aflpp(aflpp_path,executable_name,local_seeddir_path,file_mode):
-  
-  afl_fuzz_path = aflpp_path + "/afl-fuzz"
-  cmd = [afl_fuzz_path,"-i",local_seeddir_path,"-o","local_out",executable_name]
-  if file_mode == True:
-    cmd.append("@@")
-    
-    env_var = os.environ.copy()
-    env_var["AFL_SKIP_CPUFREQ"] = "1"
-    env_var["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
-    
-    if (sys.version_info < (3,3)):
-      proc = subprocess.Popen(cmd,env=env_var)
-      timer = Timer(5,proc.kill)
-      try:
-        timer.start()
-        proc.wait()
-      finally:
-        timer.cancel()
-    else:
-      proc = subproess.Popen(cmd, env=env_var,stderr=PIPE,stdout=PIPE)
-      try:
-        proc.wait(timeout=5)
-      except subprocess.TimeoutExpired:
-        proc.kill()
-        
-    return proc.returncode
+
+    afl_fuzz_path = aflpp_path + "/afl-fuzz"
+    cmd = [afl_fuzz_path,"-i",local_seeddir_path,"-o","local_out",executable_name]
+    if file_mode == True:
+      cmd.append("@@")
+
+      env_var = os.environ.copy()
+      env_var["AFL_SKIP_CPUFREQ"] = "1"
+      env_var["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
+
+      if (sys.version_info < (3,3)):
+        proc = subprocess.Popen(cmd,env=env_var)
+        timer = Timer(5,proc.kill)
+        try:
+          timer.start()
+          proc.wait()
+        finally:
+          timer.cancel()
+      else:
+        proc = subproess.Popen(cmd, env=env_var,stderr=PIPE,stdout=PIPE)
+        try:
+          proc.wait(timeout=5)
+        except subprocess.TimeoutExpired:
+          proc.kill()
+
+      return proc.returncode
  
 def main () :
   
