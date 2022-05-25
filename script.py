3b022ee9365e57fcb7d8b3f24d8792ca2454935b
Hwangwoosam <61729954+Hwangwoosam@users.noreply.github.com>
2022-05-25 14:15:51 +0900

    Update script.py

--- script.py
+++ script.py
@@ -15,26 +15,26 @@ def execute_aflpp(aflpp_path,executable_name,local_seeddir_path,file_mode):
     if file_mode == True:
       cmd.append("@@")
 
-      env_var = os.environ.copy()
-      env_var["AFL_SKIP_CPUFREQ"] = "1"
-      env_var["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
+    env_var = os.environ.copy()
+    env_var["AFL_SKIP_CPUFREQ"] = "1"
+    env_var["AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES"] = "1"
 
-      if (sys.version_info < (3,3)):
-        proc = subprocess.Popen(cmd,env=env_var)
-        timer = Timer(5,proc.kill)
-        try:
-          timer.start()
-          proc.wait()
-        finally:
-          timer.cancel()
-      else:
-        proc = subproess.Popen(cmd, env=env_var)
-        try:
-          proc.wait(timeout=5)
-        except subprocess.TimeoutExpired:
-          proc.kill()
+    if (sys.version_info < (3,3)):
+      proc = subprocess.Popen(cmd,env=env_var)
+      timer = Timer(5,proc.kill)
+      try:
+        timer.start()
+        proc.wait()
+      finally:
+        timer.cancel()
+    else:
+      proc = subproess.Popen(cmd, env=env_var)
+      try:
+        proc.wait(timeout=5)
+      except subprocess.TimeoutExpired:
+        proc.kill()
 
-      return proc.returncode
+    return proc.returncode
  
 def main () :
   
