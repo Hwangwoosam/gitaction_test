5d15204fa26fff2dfc0efce0e79191c12c9e5b6c
Hwangwoosam <sam1783@naver.com>
2022-09-05 12:38:02 +0900

    code changes

--- test/cJSON/cJSON.c
+++ test/cJSON/cJSON.c
@@ -304,7 +304,7 @@ typedef struct
 /* Parse the input text to generate a number, and populate the result into item. */
 static cJSON_bool parse_number(cJSON * const item, parse_buffer * const input_buffer)
 {   
-    // char test[] = "test2";
+    char test[] = "test2";
     exit(1);
     double number = 0;
     unsigned char *after_end = NULL;
